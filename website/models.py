f9ab75b945e576abcaa5bda8cb187ac70acf153c
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2024-08-12 15:19:28 +0200

    chore: add initial `Artist`, `Album` and `Song` models

--- website/models.py
+++ website/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    img_url = models.CharField(max_length=200)
+
+
+class Album(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.RESTRICT)
+    title = models.CharField(max_length=100)
+    img_url = models.CharField(max_length=200)
+    pub_date = models.DateField()
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    album = models.ForeignKey(Album, on_delete=models.RESTRICT)
