c4dfabcac73071437d5f7ca21a72161cacbbd316
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2024-08-13 18:11:13 +0200

    chore: add `__str__` method to models for debugging

--- website/models.py
+++ website/models.py
@@ -8,6 +8,9 @@ class Artist(models.Model):
     name = models.CharField(max_length=100)
     img_url = models.CharField(max_length=200)
 
+    def __str__(self):
+        return "{id} [{mdbId}] {name}".format(id=self.id, mdbId=self.mdbId, name=self.name)
+
 
 class Album(models.Model):
     mdbId = models.UUIDField(default=uuid.uuid4, editable=False)
@@ -16,8 +19,14 @@ class Album(models.Model):
     img_url = models.CharField(max_length=200)
     pub_date = models.DateField()
 
+    def __str__(self):
+        return "{id} [{mdbId}] {title}".format(id=self.id, mdbId=self.mdbId, title=self.title)
+
 
 class Song(models.Model):
     mdbId = models.UUIDField(default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=100)
     album = models.ForeignKey(Album, on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return "{id} [{mdbId}] {title}".format(id=self.id, mdbId=self.mdbId, title=self.title)
