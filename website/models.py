dd97b2e2b9a0f6f29079f6a853d88bd85a6f7bd7
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2024-08-13 18:04:58 +0200

    chore: add `mdbId` flield to `Artist`, `Album` and `Song` models

--- website/models.py
+++ website/models.py
@@ -1,12 +1,16 @@
+import uuid
+
 from django.db import models
 
 
 class Artist(models.Model):
+    mdbId = models.UUIDField(default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     img_url = models.CharField(max_length=200)
 
 
 class Album(models.Model):
+    mdbId = models.UUIDField(default=uuid.uuid4, editable=False)
     artist = models.ForeignKey(Artist, on_delete=models.RESTRICT)
     title = models.CharField(max_length=100)
     img_url = models.CharField(max_length=200)
@@ -14,5 +18,6 @@ class Album(models.Model):
 
 
 class Song(models.Model):
+    mdbId = models.UUIDField(default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=100)
     album = models.ForeignKey(Album, on_delete=models.RESTRICT)
