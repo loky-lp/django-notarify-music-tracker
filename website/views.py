187c8174c42502fed06e9d383ca94c3439ce569c
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2024-08-14 00:06:14 +0200

    feat: add login view

--- website/views.py
+++ website/views.py
@@ -1,6 +1,10 @@
-# from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the website index.")
+
+
+def login(request):
+    return render(request, 'website/login.html')
