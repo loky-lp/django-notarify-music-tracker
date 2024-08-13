e6fa2d220c51e085383acdb5dbfcc88b291e4472
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2024-08-13 18:37:33 +0200

    chore: enable admin editing of `Artist`, `Album` and `Song` models

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Artist, Album, Song
+
+admin.site.register(Artist)
+admin.site.register(Album)
+admin.site.register(Song)
